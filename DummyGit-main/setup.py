cd9cfd2bac9cc1ea00a0ddf29b8ec2a0ff7aa68c
sris123 <110453218+sris123@users.noreply.github.com>
2023-08-16 12:25:03 +0530

    Add files via upload

---
+++ DummyGit-main/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+import time
+import os
+time.sleep(60)
+
+setup(name='o9dummygit',
+      packages=['o9dummyfolder'],
+      include_package_data=True
+      )
